df3ece9b6b3d1c085f3284f39611a720dddc70cc
BenThinkin-Code <thenbinkin@gmail.com>
2021-04-07 21:01:27 -0400

    etch-a-sketch project final

---
+++ etch.js
@@ -0,0 +1,362 @@
+/*const container = document.querySelector('#main');
+
+const content = document.createElement('div');
+content.classList.add('box');
+content.setAttribute("id", "test");
+content.textContent = 'BOX1';
+container.appendChild(content); 
+
+const containerTwo = document.querySelector('#main');
+
+const contentTwo = document.createElement('div');
+contentTwo.classList.add('box');
+contentTwo.setAttribute("id", "testTwo");
+contentTwo.textContent = 'BOX2';
+containerTwo.appendChild(contentTwo); 
+
+const containerThree = document.querySelector('#main');
+
+const contentThree = document.createElement('div');
+contentThree.classList.add('box');
+contentThree.setAttribute("id", "testThree");
+contentThree.textContent = 'BOX3';
+containerThree.appendChild(contentThree); 
+
+const containerFour = document.querySelector('#main');
+
+const contentFour = document.createElement('div');
+contentFour.classList.add('box');
+contentFour.setAttribute("id", "testFour");
+contentFour.textContent = 'BOX4';
+containerFour.appendChild(contentFour);
+
+const containerFive = document.querySelector('#main');
+
+const contentFive = document.createElement('div');
+contentFive.classList.add('box');
+contentFive.setAttribute("id", "testFive");
+contentFive.textContent = 'BOX5';
+containerFive.appendChild(contentFive);
+
+const containerSix = document.querySelector('#main');
+
+const contentSix = document.createElement('div');
+contentSix.classList.add('box');
+contentSix.setAttribute("id", "testSix");
+contentSix.textContent = 'BOX6';
+containerSix.appendChild(contentSix);
+
+const containerSeven = document.querySelector('#main');
+
+const contentSeven = document.createElement('div');
+contentSeven.classList.add('box');
+contentSeven.setAttribute("id", "testSeven");
+contentSeven.textContent = 'BOX7';
+containerSeven.appendChild(contentSeven); 
+
+const containerEight = document.querySelector('#main');
+
+const contentEight = document.createElement('div');
+contentEight.classList.add('box');
+contentEight.setAttribute("id", "testEight");
+contentEight.textContent = 'BOX8';
+containerEight.appendChild(contentEight);
+
+const containerNine = document.querySelector('#main');
+
+const contentNine = document.createElement('div');
+contentNine.classList.add('box');
+contentNine.setAttribute("id", "testNine");
+contentNine.textContent = 'BOX9';
+containerNine.appendChild(contentNine);
+
+const containerTen = document.querySelector('#main');
+
+const contentTen = document.createElement('div');
+contentTen.classList.add('box');
+contentTen.setAttribute("id", "testTen");
+contentTen.textContent = 'BOX10';
+containerTen.appendChild(contentTen);
+
+const containerEleven = document.querySelector('#main');
+
+const contentEleven = document.createElement('div');
+contentEleven.classList.add('box');
+contentEleven.setAttribute("id", "testEleven");
+contentEleven.textContent = 'BOX11';
+containerEleven.appendChild(contentEleven); 
+
+const containerTwelve = document.querySelector('#main');
+
+const contentTwelve = document.createElement('div');
+contentTwelve.classList.add('box');
+contentTwelve.setAttribute("id", "testTwelve");
+contentTwelve.textContent = 'BOX12';
+containerTwelve.appendChild(contentTwelve);
+
+const containerThirteen = document.querySelector('#main');
+
+const contentThirteen = document.createElement('div');
+contentThirteen.classList.add('box');
+contentThirteen.setAttribute("id", "testThirteen");
+contentThirteen.textContent = 'BOX13';
+containerThirteen.appendChild(contentThirteen);
+
+const containerFourteen = document.querySelector('#main');
+
+const contentFourteen = document.createElement('div');
+contentFourteen.classList.add('box');
+contentFourteen.setAttribute("id", "testFourteen");
+contentFourteen.textContent = 'BOX14';
+containerFourteen.appendChild(contentFourteen);
+
+const containerFifteen = document.querySelector('#main');
+
+const contentFifteen = document.createElement('div');
+contentFifteen.classList.add('box');
+contentFifteen.setAttribute("id", "testFifteen");
+contentFifteen.textContent = 'BOX15';
+containerFifteen.appendChild(contentFifteen);
+
+const containerSixteen = document.querySelector('#main');
+
+const contentSixteen = document.createElement('div');
+contentSixteen.classList.add('box');
+contentSixteen.setAttribute("id", "testSixteen");
+contentSixteen.textContent = 'BOX16';
+containerSixteen.appendChild(contentSixteen);
+*/
+
+
+/*var div = document.getElementById( 'test' );
+div.onmouseover = function() {
+  this.style.backgroundColor = 'green';
+};
+div.onmouseout = function() {
+  this.style.backgroundColor = 'black';
+};
+var div = document.getElementById( 'testTwo' );
+div.onmouseover = function() {
+  this.style.backgroundColor = 'green';
+};
+div.onmouseout = function() {
+  this.style.backgroundColor = 'black';
+};
+var div = document.getElementById( 'testThree' );
+div.onmouseover = function() {
+  this.style.backgroundColor = 'green';
+};
+div.onmouseout = function() {
+  this.style.backgroundColor = 'black';
+};
+var div = document.getElementById( 'testFour' );
+div.onmouseover = function() {
+  this.style.backgroundColor = 'green';
+};
+div.onmouseout = function() {
+  this.style.backgroundColor = 'black';
+};
+var div = document.getElementById( 'testFive' );
+div.onmouseover = function() {
+  this.style.backgroundColor = 'green';
+};
+div.onmouseout = function() {
+  this.style.backgroundColor = 'black';
+};
+var div = document.getElementById( 'testSix' );
+div.onmouseover = function() {
+  this.style.backgroundColor = 'green';
+};
+div.onmouseout = function() {
+  this.style.backgroundColor = 'black';
+};
+var div = document.getElementById( 'testSeven' );
+div.onmouseover = function() {
+  this.style.backgroundColor = 'green';
+};
+div.onmouseout = function() {
+  this.style.backgroundColor = 'black';
+};
+var div = document.getElementById( 'testEight' );
+div.onmouseover = function() {
+  this.style.backgroundColor = 'green';
+};
+div.onmouseout = function() {
+  this.style.backgroundColor = 'black';
+};
+var div = document.getElementById( 'testNine' );
+div.onmouseover = function() {
+  this.style.backgroundColor = 'green';
+};
+div.onmouseout = function() {
+  this.style.backgroundColor = 'black';
+};
+var div = document.getElementById( 'testTen' );
+div.onmouseover = function() {
+  this.style.backgroundColor = 'green';
+};
+div.onmouseout = function() {
+  this.style.backgroundColor = 'black';
+};
+var div = document.getElementById( 'testEleven' );
+div.onmouseover = function() {
+  this.style.backgroundColor = 'green';
+};
+div.onmouseout = function() {
+  this.style.backgroundColor = 'black';
+};
+var div = document.getElementById( 'testTwelve' );
+div.onmouseover = function() {
+  this.style.backgroundColor = 'green';
+};
+div.onmouseout = function() {
+  this.style.backgroundColor = 'black';
+};
+var div = document.getElementById( 'testThirteen' );
+div.onmouseover = function() {
+  this.style.backgroundColor = 'green';
+};
+div.onmouseout = function() {
+  this.style.backgroundColor = 'black';
+};
+var div = document.getElementById( 'testFourteen' );
+div.onmouseover = function() {
+  this.style.backgroundColor = 'green';
+};
+div.onmouseout = function() {
+  this.style.backgroundColor = 'black';
+};
+var div = document.getElementById( 'testFifteen' );
+div.onmouseover = function() {
+  this.style.backgroundColor = 'green';
+};
+div.onmouseout = function() {
+  this.style.backgroundColor = 'black';
+};
+var div = document.getElementById( 'testSixteen' );
+div.onmouseover = function() {
+  this.style.backgroundColor = 'green';
+};
+div.onmouseout = function() {
+  this.style.backgroundColor = 'black';
+};*/
+
+function clearIt() {
+  document.getElementById('#main').removeAttribute('onmouseout');
+  console.log(click);
+ }
+
+ function createDiv() {
+  let div = document.createElement('div');
+  div.setAttribute("id", "A");
+  div.className = 'box';
+  div.innerText = document.getElementById('getText').innerText;
+  document.getElementsByClassName("grid")[0].appendChild(div);
+}
+
+
+(function() {
+  "use strict";
+
+  document.onmousemove = handleMouseMove;
+  function handleMouseMove(event) {
+    var dot, eventDoc, doc, body, pageX, pageY;
+    
+    event = event || window.event; // IE-ism
+    
+    // If pageX/Y aren't available and clientX/Y
+    // are, calculate pageX/Y - logic taken from jQuery
+    // Calculate pageX/Y if missing and clientX/Y available
+    if (event.pageX == null && event.clientX != null) {
+      eventDoc = (event.target && event.target.ownerDocument) || document;
+      doc = eventDoc.documentElement;
+      body = eventDoc.body;
+
+      event.pageX = event.clientX +
+        (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+        (doc && doc.clientLeft || body && body.clientLeft || 0);
+      event.pageY = event.clientY +
+        (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+        (doc && doc.clientTop  || body && body.clientTop  || 0 );
+    }
+
+    
+    // Add a dot to follow the cursor
+    dot = document.createElement('div');
+    dot.className = "dot";
+    dot.style.left = event.pageX + "px";
+    dot.style.top = event.pageY + "px";
+    document.querySelector(".grid").appendChild(dot);
+  }
+})();
+
+
+window.onload = function() {
+  var x = document.getElementById("div1");
+  x.onclick = function() {
+    var mvytext = prompt("Enter a Value", "0");
+    var num1 = parseInt(mvytext);
+    var parent = mvytext.parentNode;
+    var newDiv = document.createElement("div");
+    newDiv.id = "myNewDiv";
+    newDiv.className = "cdiv1";
+    
+
+
+    var txt = document.createTextNode(mvytext);
+    newDiv.appendChild(txt);
+
+    var beforeMe = document.getElementsByTagName("div")[0];
+
+    document.body.insertBefore(newDiv, beforeMe);
+
+  }
+}
+
+
+const grid = document.querySelector(".grid");
+const userInput = document.getElementById("quantity");
+const resetButton = document.querySelector(".reset");
+
+createGrid = () => {
+  for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    grid.appendChild(div);
+  }
+};
+
+updateGrid = () => {
+  grid.innerHTML = "";
+  grid.style.setProperty(
+    "grid-template-columns",
+    `repeat(${userInput.value}, 2fr)`
+  );
+  grid.style.setProperty(
+    "grid-template-rows",
+    `repeat(${userInput.value}, 2fr)`
+  );
+  for (let i = 0; i < userInput.value * userInput.value; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    grid.appendChild(div);
+  }
+  console.log(userInput.value);
+};
+
+const square = document.querySelector("div");
+square.addEventListener("mouseover", function(event) {
+  event.target.classList.replace("square", "color");
+});
+
+userInput.addEventListener("change", updateGrid);
+
+resetButton.addEventListener("click", function() {
+  grid.innerHTML = "";
+  userInput.value = "";
+  grid.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
+  grid.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
+  createGrid();
+});
+
+createGrid();
